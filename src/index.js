4210975840f46496574233034c0a79f04dbb028a
michaelscoggins <rhythmghosts@gmail.com>
2020-08-11 10:46:46 -0500

    dotenv stuff

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 // import axios here
-const axios = require('axios').default;
+const axios = require('axios').default
+const dotenv = require('dotenv')
+dotenv.config()
+const api_key = process.env.API_KEY
 
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
 // for practice purposes as we are sharing the same account
-const api_key = 'd771b19ef336ed8381def3a60b574464'
 
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
@@ -16,16 +18,14 @@ const discoverMovie = () => {
 
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
-  return axios(url)
-  .then(result => result.data)  
+  return axios(url).then(result => result.data)  
 }
 
 const getMovieByIdFailure = () => {
   const fakeId = 5783
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
   return axios(url)
-  .then(res => res.data)
-  .catch(error => error.response.status)
+  .then(res => res.data).catch(error => error.response.status)
 }
 
 module.exports = {
